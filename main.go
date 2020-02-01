2aa665ed9321a2fb551bcd29b09e927d3c2bccce
Naotsugu Someya <chome.au.lait.github@gmail.com>
2020-02-01 16:54:28 +0900

    DBからUser取得

--- main.go
+++ main.go
@@ -5,20 +5,25 @@ import (
 	"net"
 
 	"github.com/alduh-co/presence-api/config"
+	"github.com/alduh-co/presence-api/infrastructure"
 	pb "github.com/alduh-co/presence-api/proto/pb"
 	"github.com/alduh-co/presence-api/servers"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	// DB connection
+	db := infrastructure.NewDb()
+	defer db.Close()
+
+	// init gRPC server
 	listenPort, err := net.Listen("tcp", ":"+config.GetEnv("API_PORT"))
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	server := grpc.NewServer()
-
-	pb.RegisterPresenceServer(server, &servers.PresenceServer{})
+	presenceServer := &servers.PresenceServer{Db: db}
+	pb.RegisterPresenceServer(server, presenceServer)
 	if err := server.Serve(listenPort); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
--- servers/presence_server.go
+++ servers/presence_server.go
@@ -3,20 +3,31 @@ package servers
 import (
 	"context"
 
+	"github.com/alduh-co/presence-api/models"
 	pb "github.com/alduh-co/presence-api/proto/pb"
+	"github.com/jinzhu/gorm"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-type PresenceServer struct{}
-
-func (s *PresenceServer) GetUserList(ctx context.Context, req *pb.UserListRequest) (*pb.UserListResponse, error) {
-	users := []*pb.UserResponse{}
-	users = append(users, &pb.UserResponse{Name: "test1", Email: "test1@gmail.com"})
-	users = append(users, &pb.UserResponse{Name: "test2", Email: "test2@gmail.com"})
-	users = append(users, &pb.UserResponse{Name: "test3", Email: "test3@gmail.com"})
-	return &pb.UserListResponse{Users: users}, nil
+type PresenceServer struct {
+	Db *gorm.DB
 }
 
-func (s *PresenceServer) GetUser(ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+func (server *PresenceServer) GetUserList(ctx context.Context, req *pb.UserListRequest) (*pb.UserListResponse, error) {
+	responseUsers := []*pb.UserResponse{}
+	var users models.Users
+	if err := server.Db.Find(&users).Error; err != nil {
+		return nil, status.Errorf(codes.DataLoss,
+			err.Error())
+	}
+	for _, user := range users {
+		responseUsers = append(responseUsers, &pb.UserResponse{Name: user.Name, Email: user.Email})
+	}
+	return &pb.UserListResponse{Users: responseUsers}, nil
+}
+
+func (server *PresenceServer) GetUser(ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
 	// systemCode := req.GetSystemCode
 	return &pb.UserResponse{Name: "test1", Email: "test1@gmail.com"}, nil
 }
